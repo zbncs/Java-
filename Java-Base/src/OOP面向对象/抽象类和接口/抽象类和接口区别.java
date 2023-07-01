9643b108b5d4f6705e30f38fb4071210d11b0060
Zhengyi Ma <1204056646@qq.com>
2023-07-01 12:51:16 +0800

    feat: 接口和抽象类

--- Java-Base/src/OOP面向对象/抽象类和接口/抽象类和接口区别.java
+++ Java-Base/src/OOP面向对象/抽象类和接口/抽象类和接口区别.java
@@ -1,4 +1,4 @@
-package Base;
+package OOP面向对象.抽象类和接口;
 
 /**
  * 抽象类和接口区别
@@ -7,26 +7,26 @@ package Base;
  *      抽象方法可以有自己的实现方式，接口中方法只能有定义；
  *      抽象类只能单继承，接口可以多实现
  */
-public class 抽象类和接口 {
+public class 抽象类和接口区别 {
     public static void main(String[] args) {
 
     }
 }
 // 不需要，抽象类不一定非要有抽象方法。
-abstract class Demo {
+abstract class Demo1 {
     private String name;
     public static void run() {
         System.out.println("sdsd");
     }
 }
 
-interface De {
+interface De1 {
     //成员变量 final类型
     public String a = "";
     public abstract void run();
 }
 
-class A implements De {
+class A implements De1 {
 
     @Override
     public void run() {
